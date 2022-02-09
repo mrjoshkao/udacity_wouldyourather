a3a2a1008fb82b2bd2d0bc04debe14175c9b2816
mrjoshkao <joshua.kao@gmail.com>
2022-02-09 02:43:37 +0000

    finished pie chart

--- src/components/QuestionPage.js
+++ src/components/QuestionPage.js
@@ -5,11 +5,11 @@
    https://react-redux.js.org/api/hooks#usedispatch
  */
 
-import React, { useState } from 'react'
+import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { useParams } from 'react-router-dom'
-import { PieChart } from 'react-minimal-pie-chart'
-import ReactTooltip from 'react-tooltip'
+import { Checkmark } from 'react-checkmark'
+import ResultGraph from './ResultGraph.js'
 import AnswerSelector from './AnswerSelector.js'
 import { handleAnswerSubmit } from '../actions/shared'
 import { capFirstLetter } from '../utils/helpers.js'
@@ -44,72 +44,35 @@ function QuestionPage(props) {
       {
         authedUserAnswers[q.id] ?
           <div>
-            <div> {capFirstLetter(q.optionOne.text)}? </div> OR <div> {capFirstLetter(q.optionTwo.text)}? </div>
+          
+            <div style={{display:"flex",flexDirection:"row",justifyContent:"center"}}>
+              <span style={{marginTop:"-4px"}}>{capFirstLetter(q.optionOne.text)}?</span>
+              <div className="cm"><Checkmark size="small"/></div>
+            </div> 
+            OR 
+            <div> 
+              {capFirstLetter(q.optionTwo.text)}? 
+            </div>
   
             <div>
-              <ResultGraph optionOne={q.optionOne} optionTwo={q.optionTwo} userChoice={authedUserAnswers[q.id]} votesOne={votesOne} votesTwo={votesTwo}/>
+              <ResultGraph 
+                optionOne={q.optionOne} 
+                optionTwo={q.optionTwo} 
+                userChoice={authedUserAnswers[q.id]} 
+                votesOne={votesOne} votesTwo={votesTwo}
+              />
             </div>
           </div> 
           :
-          <div><AnswerSelector optionOne={capFirstLetter(q.optionOne.text)} optionTwo={capFirstLetter(q.optionTwo.text)} submitAnswer={submitAnswer}/></div>
+          <div>
+            <AnswerSelector 
+              optionOne={capFirstLetter(q.optionOne.text)} 
+              optionTwo={capFirstLetter(q.optionTwo.text)} 
+              submitAnswer={submitAnswer}
+            />
+          </div>
       }
     </div>
   )
 }
-
-function makeTooltipContent(data) {
-  const isOneHundredPercent = data[0].value===0 || data[1].value===0;
-  return (data.map((d,index)=>
-    (<div>{d.tooltip}: {d.value} {d.value===1?' vote':' votes'} <br/> {isOneHundredPercent?'(No votes for: '+ data[1-index].tooltip + ')':''}</div>)
-  ));
-}
-
-const ResultGraph = ({optionOne, optionTwo, userChoice, votesOne, votesTwo}) => {
-  const [hovered, setHovered] = useState(null);
-  const lineWidth = 60;
-  const optionOneText = capFirstLetter(optionOne.text);
-  const optionTwoText = capFirstLetter(optionTwo.text);
-  let data = [
-          { value: votesOne, color: '#E38627', tooltip: optionOneText },
-          { value: votesTwo, color: '#C13C37', tooltip: optionTwoText },
-        ]
-  const tooltips = makeTooltipContent(data);
-  data = data.filter(d=>d.value!==0);
-  
-  return(
-    <div data-tip="" data-for="chart">
-      <PieChart 
-        data={data}
-        label={({ dataEntry }) => `${Math.round(dataEntry.percentage)}%`}
-        labelPosition={100 - lineWidth / 2}
-        labelStyle={{
-          fill: '#fff',
-          opacity: 0.75,
-          pointerEvents: 'none',
-          fontSize: '10px',
-        }}
-        radius={PieChart.defaultProps.radius - 6}
-        startAngle={90}
-        lineWidth={60}
-        animate
-        onMouseOver={(_, index) => {
-          setHovered(index);
-        }}
-        onMouseOut={() => {
-          setHovered(null);
-        }}
-      />
-      <ReactTooltip
-        id="chart"
-        getContent={() =>
-          typeof hovered === 'number' ? tooltips[hovered]:null
-        }
-        multiline={true}
-      />
-      
-      <div>You Selected: {userChoice==="optionOne"?optionOneText:optionTwoText}</div>
-    </div>
-  )
-}
-
 export default QuestionPage;
---
+++ src/components/ResultGraph.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react'
+import { PieChart } from 'react-minimal-pie-chart'
+import ReactTooltip from 'react-tooltip'
+import { capFirstLetter } from '../utils/helpers.js'
+
+function makeTooltipContent(data) {
+  const hasZeroVotes = (data[0].value === 0 || data[1].value === 0) ? 
+          (data[0].value===0 ? data[0].tooltip : data[1].tooltip) 
+          : 
+          null;
+  return (data.filter(d=>d.value!==0).map((d,index)=>
+    (<div>{d.tooltip}: {d.value} {d.value===1?' vote':' votes'} <br/> {hasZeroVotes?'(No votes for: '+ hasZeroVotes + ')':''}</div>)
+  ));
+}
+
+function ResultGraph({optionOne, optionTwo, userChoice, votesOne, votesTwo}){
+  const [hovered, setHovered] = useState(null);
+  const lineWidth = 60;
+  const optionOneText = capFirstLetter(optionOne.text);
+  const optionTwoText = capFirstLetter(optionTwo.text);
+  let data = [
+          { value: votesOne, color: '#E38627', tooltip: optionOneText },
+          { value: votesTwo, color: '#C13C37', tooltip: optionTwoText },
+        ]
+  const tooltips = makeTooltipContent(data);
+  data = data.filter(d=>d.value!==0);
+  
+  return(
+    <div data-tip="" data-for="chart">
+      <PieChart 
+        data={data}
+        label={({ dataEntry }) => `${Math.round(dataEntry.percentage)}%`}
+        labelPosition={100 - lineWidth / 2}
+        labelStyle={{
+          fill: '#fff',
+          opacity: 0.75,
+          pointerEvents: 'none',
+          fontSize: '10px',
+        }}
+        radius={PieChart.defaultProps.radius - 6}
+        startAngle={90}
+        lineWidth={60}
+        animate
+        onMouseOver={(_, index) => {
+          setHovered(index);
+        }}
+        onMouseOut={() => {
+          setHovered(null);
+        }}
+      />
+      <ReactTooltip
+        id="chart"
+        getContent={() =>
+          typeof hovered === 'number' ? tooltips[hovered]:null
+        }
+        multiline={true}
+      />
+      
+      <div>You Selected: {userChoice==="optionOne"?optionOneText:optionTwoText}</div>
+    </div>
+  )
+}
+
+export default ResultGraph
